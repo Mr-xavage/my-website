d78ce4edb07034999a8036d63c4042be10bd102c
Mr-xavage <josephahadi71@gmail.com>
2025-04-16 04:18:37 +0300

    initial commit

---
+++ store/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+class StoreConfig(AppConfig):  # Change 'StoreConfig' to your actual app's name
+        default_auto_field = 'django.db.models.BigAutoField'
+        name = 'store'  # Change 'store' to your actual app name
+
+
+
+
---
+++ store/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Customer
+import json
+import datetime
+from .models import *
+from .utils import cookieCart, cartData, guestOrder
+
+
+def store(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+
+    products = Product.objects.all()
+    context = {'products': products, 'cartItems': cartItems}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+    data = cartData(request)
+    context = {'items': data['items'], 'order': data['order'], 'cartItems': data['cartItems']}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    data = cartData(request)
+    context = {'items': data['items'], 'order': data['order'], 'cartItems': data['cartItems']}
+    return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('Product:', productId)
+
+    if request.user.is_authenticated:
+        try:
+            customer = request.user.customer
+        except Customer.DoesNotExist:
+            return JsonResponse({'error': 'User has no customer profile'}, status=400)
+
+        product = Product.objects.get(id=productId)
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+        if action == 'add':
+            orderItem.quantity += 1
+        elif action == 'remove':
+            orderItem.quantity -= 1
+
+        orderItem.save()
+
+        if orderItem.quantity <= 0:
+            orderItem.delete()
+
+        return JsonResponse('Item was added', safe=False)
+
+    else:
+        return JsonResponse({'error': 'User not authenticated'}, status=401)
+
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        try:
+            customer = request.user.customer  # Try to fetch Customer
+        except Customer.DoesNotExist:
+            return JsonResponse({'error': 'Customer not found!'}, status=400)
+
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    else:
+        customer, order = guestOrder(request, data)
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+
+    return JsonResponse('Payment submitted.', safe=False)
